bf2b680b47cd2b7ebdbfffa773444ebfa950e381
adam freedman <adamfreedma@gmail.com>
2024-03-07 15:19:39 +0200

    Started obstacle and teleporters.

---
+++ teleporter.py
@@ -0,0 +1,13 @@
+from custom_types import *
+from custom_types import vector3
+from obstacle import Obstacle
+
+class Teleporter(Obstacle):
+    
+    def __init__(self, location: Tuple[float, float, float], radius: float, target: Tuple[float, float, float]) -> None:
+        super().__init__(location, radius)
+        self.__target = target
+        
+    def get_target(self) -> vector3:
+        return self.__target
+    
